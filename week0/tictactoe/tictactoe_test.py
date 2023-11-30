0c1eede715921e3af74a4990fead7a53fc29f99c
Adelaide-Baron <addi.baron@currencycloud.com>
2023-11-30 22:07:36 +0000

    test and update actions

--- week0/tictactoe/tictactoe_test.py
+++ week0/tictactoe/tictactoe_test.py
@@ -1,5 +1,6 @@
 import unittest
 from tictactoe import player
+from tictactoe import actions
 
 class TestBoardHelpers(unittest.TestCase):
     def setUp(self):
@@ -23,9 +24,20 @@ class TestBoardHelpers(unittest.TestCase):
                   [self.X, self.X, self.X]]
 
     def test_player(self):
-        self.assertEqual(player(self.empty_board), self.X)
-        self.assertEqual(player(self.X_next), self.X)
-        self.assertEqual(player(self.O_next), self.O)
+        self.assertEqual(self.X,player(self.empty_board))
+        self.assertEqual(self.X, player(self.X_next))
+        self.assertEqual(self.O, player(self.O_next))
+
+    def test_empty_actions_returns_all_cells(self):
+        actual_results = actions(self.empty_board)
+        expected_results = {(0,0),(0, 1), (0,2), (1, 0),(1,1) ,(1, 2),(2,0), (2,1),(2, 1), (2, 2)}
+        self.assertEqual(expected_results, actual_results)
+
+    def test_actions_returns_all_cells(self):
+        actual_results = actions(self.O_next)
+        expected_results = {(0, 1), (1, 0), (1, 2), (2, 1)}
+        self.assertEqual(expected_results, actual_results)
+        # flesh this test out
 
 if __name__ == '__main__':
     unittest.main()
