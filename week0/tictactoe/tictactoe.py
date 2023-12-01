939b09a197c23748cd0b12192d7d14ac872bd6c3
Adelaide-Baron <addi.baron@currencycloud.com>
2023-12-01 22:46:53 +0000

    refactor & cleanup

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -59,12 +59,6 @@ def result(board, action):
     return board
     raise NotImplementedError
 
-
-# The winner function should accept a board as input, and return the winner of the board if there is one.
-# If the X player has won the game, your function should return X. If the O player has won the game, your function should return O.
-# One can win the game with three of their moves in a row horizontally, vertically, or diagonally.
-# You may assume that there will be at most one winner (that is, no board will ever have both players with three-in-a-row, since that would be an invalid board state).
-# If there is no winner of the game (either because the game is in progress, or because it ended in a tie), the function should return None.
 def winner(board):
     """
     Returns the winner of the game, if there is one.
@@ -105,49 +99,17 @@ def check_win_for_given_player(board, player_to_check):
     else:
         possible_wins = get_possible_moves(possible_wins, (1, 1), False)
 
-    for combination in possible_wins: #don't feel this properly enumerates over what gets updated in lower lines
-        i = combination[0][0]
-        j = combination[0][1]
-        if board[i][j] == player_to_check:
-            # check if winner
-            i = combination[1][0]
-            j = combination[1][1]
-            if board[i][j] == player_to_check:
-                i = combination[2][0]
-                j = combination[2][1]
-                if board[i][j] == player_to_check:
-                    print('WINNING COMBO FOUND')
-                    winner = True
-                    break
-                else:
-                    possible_wins = get_possible_moves(possible_wins, (i, j), False)
-                    break #is necessary?
-            else:
+    for combination in possible_wins:
+        for pos in combination:
+            i, j = pos
+            if board[i][j] != player_to_check:
                 possible_wins = get_possible_moves(possible_wins, (i, j), False)
-                break #is necessary?
-            # otherwise, remove this & other cells from the possible_moves
-            # possible_wins = get_possible_moves(possible_wins, (i, j), True)
+                break
         else:
-            possible_wins = get_possible_moves(possible_wins, (i, j), False)
+            print('WINNING COMBO FOUND')
+            winner = True
+
     return winner
-    # print("HI {}".format(len(possible_wins)))
-    # # check combination for winning combo
-    # for combination in possible_wins:
-    #
-    #
-    #
-    # if(len(possible_wins) ==1):
-    #     print(player_to_check)
-    #     print(possible_wins)
-    #     winning_combo = True
-    #     combination = possible_wins[0]
-    #     for cell in combination:
-    #         i = cell[0]
-    #         j = cell[1]
-    #         if board[i][j] != player_to_check:
-    #             winning_combo = False
-    #             break
-    #     return winning_combo
 
 def get_possible_moves(possible_moves, cell, is_populated):
     moves_to_return = list()
